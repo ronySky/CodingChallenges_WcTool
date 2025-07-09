f55f1c0b108ef1ffe42186683c99c2c65cbb9dfb
Rony Hotimsky <rony.hotimsky@gmail.com>
2025-07-09 10:19:33 -0300

    Feat: Adding argument parser

--- src/main/java/com/ronysky/Ccwc.java
+++ src/main/java/com/ronysky/Ccwc.java
@@ -1,7 +1,105 @@
 package com.ronysky;
 
-public class Main {
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+public class Ccwc {
+
+    private WordCounterService service;
+    private WordCounterParams params;
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Ccwc ccwc = new Ccwc();
+        ccwc.params = ccwc.parseArgs(args);
+        ccwc.service = new WordCounterService(ccwc.params.text);
+        String result = ccwc.count(ccwc.params);
+        System.out.println(result);
+    }
+
+    private WordCounterParams parseArgs(String[] args) {
+        List<String> argsList = new ArrayList<>(Arrays.asList(args));
+        params = new WordCounterParams();
+        parseFlags(argsList, params);
+        parseText(argsList, params);
+
+        return params;
+    }
+
+    private void parseFlags(List<String> argsList, WordCounterParams params) {
+        if (argsList.contains("-c")) {
+            params.countBytes = true;
+            argsList.remove("-c");
+        }
+        if (argsList.contains("-m")) {
+            params.countChars = true;
+            argsList.remove("-m");
+        }
+        params.countLines = argsList.removeIf("-l"::equals);
+        params.countWords = argsList.removeIf("-w"::equals);
+        //If no arguments are provided, all of them expect countChars are set to true
+        if (!params.countBytes && !params.countChars && !params.countLines && !params.countWords) {
+            params.countBytes = params.countLines = params.countWords = true;
+        }
+    }
+
+    private void parseText(List<String> argsList, WordCounterParams params) {
+        if (argsList.size() > 1) {
+            throw new IllegalArgumentException("Unknown parameters:" + String.join(" ", argsList));
+        }
+
+        if (argsList.isEmpty()) {
+            try {
+                params.text = new String(System.in.readAllBytes());
+            } catch (IOException e) {
+                throw new RuntimeException("Error reading from stdin");
+            }
+        } else {
+            String filename = argsList.get(0);
+            if (!Files.exists(Path.of(filename))) {
+                throw new IllegalArgumentException("Filename %s not found".formatted(filename));
+            }
+            try {
+                params.text = Files.readString(Path.of(filename));
+                params.filename = filename;
+            } catch (IOException e) {
+                throw new RuntimeException("Error reading from" + filename);
+            }
+        }
+    }
+
+    public String count(WordCounterParams params) {
+        List<String> result = new ArrayList<>();
+        if (params.countLines) {
+            result.add(String.valueOf(service.countLines()));
+        }
+        if (params.countWords) {
+            result.add(String.valueOf(service.countWords()));
+        }
+        if (params.countBytes) {
+            result.add(String.valueOf(service.countBytes()));
+        }
+        if (params.countChars) {
+            result.add(String.valueOf(service.countChars()));
+        }
+        if (params.filename != null) {
+            result.add(params.filename);
+        }
+        return String.join("\t", result);
+    }
+
+    private static class WordCounterParams {
+        boolean countBytes;
+        boolean countChars;
+        boolean countLines;
+        boolean countWords;
+        String filename;
+        String text;
     }
 }
---
+++ src/test/java/com/ronysky/CcwcTest.java
@@ -0,0 +1,74 @@
+package com.ronysky;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class CcwcTest {
+    private static final String FILENAME = "src/main/resources/test.txt";
+    private ByteArrayOutputStream output;
+
+    @BeforeEach
+    void setup() {
+        output = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(output));
+    }
+
+    @Test
+    void When_FileDoesNotExist_Then_ThrowException() {
+        assertThrows(IllegalArgumentException.class, () -> Ccwc.main(new String[]{"test.tx"}));
+    }
+
+    @Test
+    void When_OnlyByteFlagIsPresent_Then_OnlyPrintByteCount() {
+        Ccwc.main(new String[]{"-c", FILENAME});
+        assertEquals(formatOutputString("342190\t"), output.toString());
+    }
+
+    @Test
+    void When_OnlyLineFlagIsPresent_Then_OnlyPrintLineCount() {
+        Ccwc.main(new String[]{"-l", FILENAME});
+        assertEquals(formatOutputString("7145\t" ), output.toString());
+    }
+
+    @Test
+    void When_OnlyWordFlagIsPresent_Then_OnlyPrintWordCount() {
+        Ccwc.main(new String[]{"-w", FILENAME});
+        assertEquals(formatOutputString("58164\t" ), output.toString());
+    }
+
+    @Test
+    void When_OnlyCharFlagIsPresent_Then_OnlyPrintCharCount() {
+        Ccwc.main(new String[]{"-m", FILENAME});
+        assertEquals(formatOutputString("339292\t" ), output.toString());
+    }
+
+    @Test
+    void When_MultipleFlagsArePresent_Then_PrintEveryTrueFlagCount() {
+        Ccwc.main(new String[]{"-c", "-l", "-m", FILENAME});
+        assertEquals(formatOutputString("7145\t342190\t339292\t" ), output.toString());
+    }
+
+    @Test
+    void When_NoFlagsArePresent_ThenPrintLineWordCharCount() {
+        Ccwc.main(new String[]{FILENAME});
+        assertEquals(formatOutputString("7145\t58164\t342190\t"), output.toString());
+    }
+
+    @Test
+    void When_NoFilenameIsPresent_Then_ReadFromSystemIn() {
+        System.setIn(new ByteArrayInputStream("ABCDE\nF".getBytes()));
+        Ccwc.main(new String[]{"-l"});
+        assertEquals("2\n", output.toString());
+    }
+
+    private String formatOutputString(String prefix) {
+        return prefix + FILENAME + "\n";
+    }
+}
